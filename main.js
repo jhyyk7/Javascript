7f06cb83a53bbaed9cb9ddfe9311dc81419d6a10
jhyyk7 <jhyyk7@gmail.com>
2019-10-01 17:07:40 +0900

    Add main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+var path = require ('path')
+var express = require ('express')
+var app = express();
+
+app.use (express.static(path.join('public')))
+
+app.get ('/', function (request, response){
+    response.sendfile(path.join('./htmlFolder/index.html'))
+})
+
+app.get('/page/:pageid', function(request, response){
+    
+    response.sendfile(path.join(`./htmlFolder/${request.params.pageid}.html`))
+})
+app.listen(3000);
