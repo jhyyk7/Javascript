3b6363535cb960aac1ce609fbec95fbf03a425d0
jhyyk7 <jhyyk7@gmail.com>
2019-10-03 20:48:30 +0900

    using jquery and refactoring

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ var app = express();
 
 app.use (express.static(path.join('public')))
 
+
 app.get ('/', function (request, response){
     response.sendfile(path.join('./htmlFolder/index.html'))
 })
-
 app.get('/page/:pageid', function(request, response){
     
     response.sendfile(path.join(`./htmlFolder/${request.params.pageid}.html`))
 })
+
+
 app.get ('/testpage', function (request, response){
     response.sendfile(path.join('./htmlFolder/testpage.html'))
 })
